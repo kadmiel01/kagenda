2ac5a0b05fd9f1b49bdb5e0bd06585f135b7d806
kadmiel01 <cadmielcamilo01@gmail.com>
2018-09-13 19:43:20 -0300

    Criando modelo de comentario

--- events/models.py
+++ events/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.utils import timezone
+from libgravatar import Gravatar
 
 class Event(models.Model):
 
@@ -7,8 +8,8 @@ class Event(models.Model):
         ('0','Sem prioridade'),
         ('1','Normal'),
         ('2','Urgente'),
-        ('3', 'Muito Urgente'),
-        ('4', 't Ultra Mega Hiper Urgente')
+        ('3','Muito Urgente'),
+        ('4','t Ultra Mega Hiper Urgente')
     )
 
     date = models.DateField()
@@ -18,4 +19,20 @@ class Event(models.Model):
     def __str__(self):
         return self.event
 
-# Create your models here.
+class Coment(models.Model):
+    """comentário efeturado em um determidado evento."""
+
+    author = models.CharField(max_length=80)
+    email = models.EmailField()
+    text = models.CharField(max_length=160)
+    commented = models.DateTimeField(default=timezone.now)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="comment_event")
+
+    """"Retorna a partir do endereço de email, um avatar configurado no Gravatar"""
+    def avatar(self):
+        g = Gravatar(self.email)
+        return g.get_image(default='identicon')
+
+    def __str__(self):
+        return"{} comentou em {:%c}".format(self.author, self.commented)
+
